6885a5a9a53adbaf1cca83bfec8c751b6184a195
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-05-26 21:24:55 -0300

    Criando a paginação

--- src/controllers/refound-controller.ts
+++ src/controllers/refound-controller.ts
@@ -42,7 +42,48 @@ class RefoundsController {
 
     async index(request: Request, response: Response){
 
-        response.status(200).json({message: "vendo todos"})
+        const querySchema = z.object({
+            name: z.string().optional().default(""),
+            page: z.coerce.number().optional().default(1),
+            perPage: z.coerce.number().optional().default(10)
+        })
+
+        const {name,page,  perPage} = querySchema.parse(request.query)
+
+        /* Calculando o valor do skip */
+
+        const skip = (page - 1) * perPage
+
+        const refunds = await prisma.refunds.findMany({
+            skip,
+            take: perPage,
+            where: {
+                user: {
+                    name: {
+                        contains: name.trim()
+                    }
+                }
+            },
+            orderBy:{createdAt: "desc"},
+            include: {
+                user: true
+            }
+        })
+
+        // Obter o total de registros para calcular o número de páginas.
+        const totalRecords = await prisma.refunds.count({
+            where: {
+                user:{
+                    name:{
+                        contains: name.trim()
+                    }
+                }
+            }
+        })
+
+        const totalPages = Math.ceil(totalRecords / perPage)
+
+        response.status(200).json({refunds, pagination: {page, perPage, totalRecords, totalPages: totalPages > 0 ? totalPages: 1}})
     }
 }
 
